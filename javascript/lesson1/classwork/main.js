d04d52fcf0be0fc8540a0f71b70198802be9e1ce
BogdanSydir <taote92@gmail.com>
2022-02-01 13:04:59 +0200

    ++ homewok classwork

---
+++ javascript/lesson1/classwork/main.js
@@ -0,0 +1,34 @@
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let result = (numbers[0] + numbers[1] + numbers[2] + numbers[3] + numbers[4] + numbers[5] + numbers[6] + numbers[7] + numbers[8] + numbers[9]);
+console.log(result);
+
+let book1 = {
+    name: 'some name',
+    numberOfPages: 50,
+    genre: 'some genre'
+}
+let book2 = {
+    name: 'some name',
+    numberOfPages: 50,
+    genre: 'some genre',
+    authors: 'some authors'
+}
+let books = [book1, book2];
+console.log(books[0])
+console.log(books[1])
+
+let height = 23;
+let width = 10;
+let s = (height * width)
+console.log(s)
+
+let dC = 4;
+let heigthC = 10;
+let sC = (3.14 * (dC / 2)**2)
+let v = (sC*heigthC)
+console.log(v)
+
+let n = 3;
+let m = 4;
+let k = Math.sqrt(n*n+m*m)
+console.log(k)
---
+++ javascript/lesson1/homework/main.js
@@ -0,0 +1,90 @@
+var word1 = 'hello';
+var word2 = 'owu';
+var word3 = 'com';
+var word4 = 'ua';
+var num1 = 1;
+var num2 = 10;
+var num3 = -999;
+var num4 = 123;
+var num5 = 3.14;
+var num6 = 2.7;
+var num7 = 16;
+var bull1 = true;
+var bull2 = false;
+
+console.log(word1);
+console.log(word2);
+console.log(word3);
+console.log(word4);
+alert(num1);
+alert(num2);
+alert(num3);
+alert(num4);
+alert(num5);
+alert(num6);
+alert(num7);
+document.write(bull1)
+document.write(bull2)
+
+var word1 = 'hell';
+var word2 = 'owuwu';
+var word3 = 'comm';
+var word4 = 'lol';
+var num1 = 5;
+var num2 = 15;
+var num3 = -994;
+var num4 = 128;
+var num5 = 8.14;
+var num6 = 7.7;
+var num7 = 21;
+var bull1 = false;
+var bull2 = true;
+
+console.log(word1);
+console.log(word2);
+console.log(word3);
+console.log(word4);
+alert(num1);
+alert(num2);
+alert(num3);
+alert(num4);
+alert(num5);
+alert(num6);
+alert(num7);
+document.write(bull1);
+document.write(bull2);
+
+let firstName = 'Богдан '
+let middleName = 'Сидір '
+let lastName = 'Ярославович'
+
+let person = [firstName, middleName, lastName]
+
+console.log(person)
+
+let first = prompt('імя');
+let second = prompt('прізвище');
+let age = prompt('вік');
+
+document.write('Вітаю ', first, second, '. Тобі ', age, ' років.');
+
+
+
+let a = 100;
+let b = '100';
+let c = true;
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+
+console.log(5 < 6);
+console.log(5 > 6);
+console.log(5 === 6);
+console.log(5 == 6);
+console.log(10 === 10);
+console.log(10 >= 10);
+console.log(10 !== 10);
+console.log(10 > 10);
+console.log(10 < 10);
+console.log(123 !== 123);
+console.log(123 == '123');
